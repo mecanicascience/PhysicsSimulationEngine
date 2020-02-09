5c7d1745eaee5aa42f3ec31be316330980be97dd
xam4lor <xam4lor@gmail.com>
2020-02-09 20:33:58 +0100

    Corrected Vector bug while drawing.

--- dev/instance/src/SObject.js
+++ dev/instance/src/SObject.js
@@ -1,13 +1,16 @@
 class SObject {
     constructor(x0, y0, positionName, vectorName) {
-        this.point = new Point(x0, y0, "red", positionName, undefined, vectorName);
+        this.pos = new Vector(x0, y0);
     }
 
     update(dt, everyObjects) {
-        this.point.pos.rotate(0.01);
+
     }
 
     draw(drawer) {
-        this.point.draw(drawer, false);
+        drawer.ellipse(this.pos.x, this.pos.y, 5, 5);
+
+        this.pos.draw();
+        this.pos.draw(this.pos);
     }
 }
