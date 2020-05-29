03a1a64643d405bf0e7bff116fdd09a0d20020b4
xam4lor <xam4lor@gmail.com>
2020-05-29 23:43:36 +0200

    Added new example.

--- dev/src/Plot.js
+++ dev/src/Plot.js
@@ -1,24 +1,13 @@
 class Plot {
     constructor() {
-        //this.pointsCarre = [new pSPoint(-1, 1), new pSPoint(1, 1), new pSPoint(1, -1), new pSPoint(-1, -1)];
-        // this.anim = pSAnimation.new('interpolation', 5, this.points);
-
-        this.anim = pSAnimation.new('easeInOutCubic', 5, {min : -5,  max : 5});
-        this.anim.start();
-
-        this.point = new pSPoint(0, 0)
+        this.tex = new pSText('\\sum \\sqrt{x^2+1} + 6 x \\text{ salut}', new Vector(), 2, 'red');
     }
 
     update(dt) {
-        this.point.pos.y = this.anim.updateKey();
+
     }
 
-    draw() {
-        // this.anim.drawKey();
-
-        this.point.draw();
-        // for (let i = 0; i < this.points.length; i++) {
-        //     this.points[i].draw();
-        // }
+    draw(drawer) {
+        this.tex.draw();
     }
 }
