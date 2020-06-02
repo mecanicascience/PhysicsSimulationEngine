807292f057a391e98daf889fd1adeb35b0db37c6
Victor Rodriguez <victorodriguez148@gmail.com>
2020-06-02 13:54:08 +0200

    Because Javascript

--- examples/bouncing_balls/src/sketch.js
+++ examples/bouncing_balls/src/sketch.js
@@ -1,10 +1,7 @@
 
 function runSimulator(simulator)
 {
-	simulator.setEngineConfig
-	(
-		(engineConf) =>
-		{
+	simulator.setEngineConfig((engineConf) => {
 			engineConf.plotter.scale = { x: 500, y: 500 };
 		}
 	).addObjects(pSObject, 10, ["_RUN_F", random, -20, 20], ["_RUN_F", random, -5, 5], 300);
