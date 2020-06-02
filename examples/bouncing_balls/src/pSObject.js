dfaa01f9210c1f4db93052836107594f48c14019
Victor Rodriguez <victorodriguez148@gmail.com>
2020-06-02 13:58:39 +0200

    Because Javascript (2)

--- examples/bouncing_balls/src/pSObject.js
+++ examples/bouncing_balls/src/pSObject.js
@@ -1,46 +1,38 @@
 
-class pSObject
-{
-   constructor(x0, y0, v0)
-   {
+class pSObject {
+   constructor(x0, y0, v0) {
       this.pos = new Vector(x0, y0);
       this.vel = new Vector(v0, 0);
       this.bounceCount = 0;
    }
 
-   update(dt)
-   {
+   update(dt) {
       this.pos.add((this.vel.copy()).mult(dt));
 
       //If the balls are out of the bounds, then they bounce
-      if (this.pos.x > _pSimulationInstance.getEngineConfig().plotter.scale.x || this.pos.x < -_pSimulationInstance.getEngineConfig().plotter.scale.x)
-      {
+      if (this.pos.x > _pSimulationInstance.getEngineConfig().plotter.scale.x || this.pos.x < -_pSimulationInstance.getEngineConfig().plotter.scale.x) {
         this.vel.x = -this.vel.x;
 
         //While bounceCount < 3, the balls bounce in a random direction
-        if(this.bounceCount < 3)
-        {
+        if(this.bounceCount < 3) {
           this.vel.y = Math.round(Math.random() * 100);
         }
 
-        ++this.bounceCount;
+        this.bounceCount++;
       }
 
-      if(this.pos.y > _pSimulationInstance.getEngineConfig().plotter.scale.y || this.pos.y < -_pSimulationInstance.getEngineConfig().plotter.scale.y)
-      {
-        if(this.bounceCount < 3)
-        {
+      if(this.pos.y > _pSimulationInstance.getEngineConfig().plotter.scale.y || this.pos.y < -_pSimulationInstance.getEngineConfig().plotter.scale.y) {
+        if(this.bounceCount < 3) {
           this.vel.x = Math.round(Math.random() * 100);
         }
 
         this.vel.y = -this.vel.y;
 
-        ++this.bounceCount;
+        this.bounceCount++;
       }
    }
 
-   draw(drawer)
-   {
+   draw(drawer) {
       drawer.noStroke().fill('red').ellipse(this.pos.x, this.pos.y, 5, 5);
    }
 }
